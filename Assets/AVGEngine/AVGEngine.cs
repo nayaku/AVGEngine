ab77625cda581f82c8b9e5c33b8379d6001794fd
zhangmingkai <zhangmingkai@cyou-inc.com>
2023-11-29 17:34:51 +0800

    装载主剧本

--- Assets/AVGEngine/AVGEngine.cs
+++ Assets/AVGEngine/AVGEngine.cs
@@ -1,4 +1,4 @@
-using GalgameNovelScript;
+﻿using GalgameNovelScript;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,25 +6,28 @@ using System.IO;
 using TMPro;
 using UnityEngine;
 
-public class AVGEngine : MonoBehaviour
+namespace AVGEngine
 {
-    public TextMeshPro dialog;
-    // Start is called before the first frame update
-    void Start()
+    public class AVGEngine : MonoBehaviour
     {
-        var text = File.ReadAllText(Application.dataPath + "/������.txt") + "\n";
+        public Canvas canvas;
+        // Start is called before the first frame update
+        void Start()
+        {
+        var text = File.ReadAllText(Application.dataPath + "/主剧本.txt") + "\n";
         var lexer = new Lexer(text);
         var parser = new Parser(lexer);
         var tree = parser.Parse();
 
         var interpreter = new Interpreter(tree);
-        interpreter.AddToGlobalScope("���", new Action<object>(Debug.Log));
+        interpreter.AddToGlobalScope("输出", new Action<object>(Debug.Log));
         interpreter.Interpret();
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
